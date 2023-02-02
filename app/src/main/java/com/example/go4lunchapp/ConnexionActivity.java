cb0eec13c773b8e27d8ceefd1c1e364b1ce2000b
Ricardo <bglennrichard.pro@yahoo.com>
2023-02-02 23:32:39 +0100

    Facebook Auth

--- app/src/main/java/com/example/go4lunchapp/ConnexionActivity.java
+++ app/src/main/java/com/example/go4lunchapp/ConnexionActivity.java
@@ -2,13 +2,24 @@ package com.example.go4lunchapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 
 public class ConnexionActivity extends AppCompatActivity {
 
     private Button btn_facebook;
     private Button btn_google;
+    //Facebook Auth values
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +28,34 @@ public class ConnexionActivity extends AppCompatActivity {
 
         btn_facebook = findViewById(R.id.btn_connexion_facebook);
         btn_google = findViewById(R.id.btn_connexion_google);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        btn_facebook.setOnClickListener(view -> facebookAuth());
+    }
+    private void facebookAuth(){
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Toast.makeText(getApplicationContext(),"Connexion réussie",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(getApplicationContext(),"Connexion annulée",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Toast.makeText(getApplicationContext(),exception.toString(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
