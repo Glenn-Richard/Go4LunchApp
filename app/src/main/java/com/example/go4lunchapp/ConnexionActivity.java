b85307eb01aa50ff28df8c9141f278fd51dca62a
Ricardo <bglennrichard.pro@yahoo.com>
2023-02-03 00:13:25 +0100

    Google Auth

--- app/src/main/java/com/example/go4lunchapp/ConnexionActivity.java
+++ app/src/main/java/com/example/go4lunchapp/ConnexionActivity.java
@@ -1,37 +1,44 @@
 package com.example.go4lunchapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class ConnexionActivity extends AppCompatActivity {
 
-    private Button btn_facebook;
-    private Button btn_google;
+    private final static int RC_SIGNIN = 100;
     //Facebook Auth values
     private CallbackManager callbackManager;
+
+    private GoogleSignInClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
 
-        btn_facebook = findViewById(R.id.btn_connexion_facebook);
-        btn_google = findViewById(R.id.btn_connexion_google);
+        Button btn_facebook = findViewById(R.id.btn_connexion_facebook);
+        Button btn_google = findViewById(R.id.btn_connexion_google);
 
         callbackManager = CallbackManager.Factory.create();
 
+        google_request();
+
         btn_facebook.setOnClickListener(view -> facebookAuth());
+
+        btn_google.setOnClickListener(view -> google_signIn());
     }
     private void facebookAuth(){
         LoginManager.getInstance().registerCallback(callbackManager,
@@ -52,10 +59,28 @@ public class ConnexionActivity extends AppCompatActivity {
                     }
                 });
     }
+    private void google_request(){
+        //Google Auth values
+        GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
 
+        client = GoogleSignIn.getClient(this, options);
+    }
+    private void google_signIn(){
+        Intent signIn = client.getSignInIntent();
+        startActivityForResult(signIn,RC_SIGNIN);
+    }
+    private void mainActivity() {
+        startActivity(new Intent(ConnexionActivity.this,MainActivity.class));
+    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGNIN) {
+            mainActivity();
+        }
     }
 }
